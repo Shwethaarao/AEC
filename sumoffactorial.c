83f2a67af4d72bd22087c2906a210ae09eaee12b
Shwetha K S <109526980+Shwethaarao@users.noreply.github.com>
2023-01-31 19:55:22 +0530

    Add files via upload

---
+++ sumoffactorial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   int n,i,fact=1,sum=0;
+   printf("enter n");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       fact=fact*i;
+       sum=sum+fact;
+   }
+   printf("%d",sum);
+}
---
+++ sumofnaturalno.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    int i,n,sum=0;
+    printf("enter a number");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        sum=sum+i;
+    }
+    printf("sum is %d",sum);
+}
