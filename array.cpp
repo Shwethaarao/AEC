adf9feec379fb7557d476257207e45df8e450ad8
Shwetha K S <109526980+Shwethaarao@users.noreply.github.com>
2023-02-01 14:43:23 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -9,9 +9,9 @@ int main(){
     cin>>a[i];
     cout<<"given array"<<endl;
     for(int i=0;i>n;i++)
-    cout>>a[i];
+    cout<<a[i];
     cout<<"reverse ordered array"<<endl;
     for(int i=n;i>0;i--)
-    cout>>a[i];
+    cout<<a[i];
     return 0;
 }
